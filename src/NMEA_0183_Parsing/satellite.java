89a3d7f6605a080b889fb34e23a87231c1b584c4
Slaptop\Slavkowski <szweds@poczta.onet.pl>
2017-08-30 01:56:20 +0200

    30082017

--- src/NMEA_0183_Parsing/satellite.java
+++ src/NMEA_0183_Parsing/satellite.java
@@ -4,6 +4,5 @@ public  abstract class satellite {
     protected String id;
     protected String azimuth;
     protected String elevation;
-    protected boolean inView;
     protected boolean usedToFix;
 }
